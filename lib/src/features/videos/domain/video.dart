f20cef10b67f154807904034315be446d495519d
AndresLince <lynxdarmatron@gmail.com>
2024-01-08 15:07:27 -0500

    Add video model

---
+++ lib/src/features/videos/domain/video.dart
@@ -0,0 +1,12 @@
+class Video {
+  final String name;
+  final String channelName;
+  final String channelImage;
+  final String views;
+  Video({
+    required this.name,
+    required this.channelName,
+    required this.views,
+    required this.channelImage,
+  });
+}
