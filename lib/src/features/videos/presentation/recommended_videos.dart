4781c27f9dee609d295e23ebc013df5989ff3e54
AndresLince <lynxdarmatron@gmail.com>
2024-01-08 12:15:09 -0500

    Add recommended_videos work in progress

---
+++ lib/src/features/videos/presentation/recommended_videos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecommendedVideos extends StatefulWidget {
+  const RecommendedVideos({super.key});
+
+  @override
+  State<RecommendedVideos> createState() => _RecommendedVideosState();
+}
+
+class _RecommendedVideosState extends State<RecommendedVideos> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
