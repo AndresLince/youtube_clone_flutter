8b7d79868eed067bbe6fbc3f5d96903f76f1cd2c
AndresLince <lynxdarmatron@gmail.com>
2024-01-09 06:46:01 -0500

    Add channelName and views to video_tile

--- lib/src/features/videos/presentation/video_tile.dart
+++ lib/src/features/videos/presentation/video_tile.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 
 class VideoTile extends StatelessWidget {
   final String name;
+  final String channelName;
+  final String views;
   VideoTile({
-    required this.name
+    required this.name,
+    required this.channelName,
+    required this.views,
   });
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(name),
+      subtitle: Text('$channelName · $views'),
     );
   }
 }
