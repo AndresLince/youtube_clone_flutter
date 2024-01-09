86ee3784b5aefd9fac78b61796c80fdf264aac73
AndresLince <lynxdarmatron@gmail.com>
2024-01-09 06:10:00 -0500

    Add mock data to _videos WIP

--- lib/src/features/videos/application/video_service.dart
+++ lib/src/features/videos/application/video_service.dart
@@ -3,5 +3,18 @@ import 'package:flutter/material.dart';
 import '../domain/video.dart';
 
 class VideoService extends ChangeNotifier{
-  List<Video> _videos = [];
+  List<Video> _videos = [
+    Video(
+      name: 'DevTalles PodCast - 147: Inicio 2024 | ¿Cómo hacer que te pasen cosas buenas?',
+      channelName: 'Fernando Herrera',
+      channelImage: 'https://yt3.ggpht.com/ytc/AIf8zZRUdgAqLyjIRxhCGaw_4Ln_awajey3DphahM-pQsg=s48-c-k-c0x00ffffff-no-rj',
+      views: '2,1 k visualizaciones',
+    ),
+    Video(
+      name: 'DevTalles PodCast - 146: Fin del 2023 | Comparativa contra ti mismo',
+      channelName: 'Fernando Herrera',
+      channelImage: 'https://yt3.ggpht.com/ytc/AIf8zZRUdgAqLyjIRxhCGaw_4Ln_awajey3DphahM-pQsg=s48-c-k-c0x00ffffff-no-rj',
+      views: '2,3 k visualizaciones',
+    ),
+  ];
 }
