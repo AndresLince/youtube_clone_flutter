7fa45bd4d43a7205de66280011f5f2f8d36ddd24
AndresLince <lynxdarmatron@gmail.com>
2024-01-14 20:18:45 -0500

    Convert RecommendedVideos widget to ConsumerWidget

--- lib/src/features/videos/presentation/recommended_videos.dart
+++ lib/src/features/videos/presentation/recommended_videos.dart
@@ -1,33 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:youtube_clone_flutter/src/features/videos/application/video_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone_flutter/src/features/videos/data/video_provider.dart';
 import 'package:youtube_clone_flutter/src/features/videos/presentation/video_tile.dart';
-
 import '../domain/video.dart';
 
-class RecommendedVideos extends StatefulWidget {
+class RecommendedVideos extends ConsumerWidget {
   const RecommendedVideos({super.key});
 
   @override
-  State<RecommendedVideos> createState() => _RecommendedVideosState();
-}
+  // Notice how "build" now receives an extra parameter: "ref"
+  Widget build(BuildContext context, WidgetRef ref) {
+    // We can use "ref.watch" inside our widget like we did using "Consumer"
+    final AsyncValue<List<Video>> videos = ref.watch(videoProvider);
 
-class _RecommendedVideosState extends State<RecommendedVideos> {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<VideoService>(builder: (context, videoService, child) {
-      return ListView.builder(
-        itemBuilder: (context, index) {
-          Video video = videoService.getVideo(index);
-          return VideoTile(
-            name: video.name,
-            channelName: video.channelName,
-            views: video.views,
-            channelImage: video.channelImage,
-          );
-        },
-        itemCount: videoService.videosCount,
-      );
-    });
+    // The rendering logic stays the same
+    return Center(
+      /// Since network-requests are asynchronous and can fail, we need to
+      /// handle both error and loading states. We can use pattern matching for this.
+      /// We could alternatively use `if (activity.isLoading) { ... } else if (...)`
+      child: switch (videos) {
+        AsyncData(:final value) => ListView.builder(
+          itemBuilder: (context, index) {
+            Video video = value[index];
+            return VideoTile(
+              name: video.name,
+              channelImage: video.channelImage,
+              views: video.views,
+              channelName: video.channelName,
+            );
+          },
+          itemCount: value.length,
+        ),
+        AsyncError() => const Text('Oops, something unexpected happened'),
+        _ => const CircularProgressIndicator(),
+      },
+    );
   }
 }
