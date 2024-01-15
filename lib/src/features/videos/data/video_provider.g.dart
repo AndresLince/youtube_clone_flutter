e967d6298545379253342bb56f7fc794d7141d59
AndresLince <lynxdarmatron@gmail.com>
2024-01-14 20:00:22 -0500

    Add video provided generated file

---
+++ lib/src/features/videos/data/video_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'video_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$videoHash() => r'49598cd1f631398f36f367985055f2eabc9052b2';
+
+/// This will create a provider named `videoProvider`
+/// which will cache the result of this function.
+///
+/// Copied from [video].
+@ProviderFor(video)
+final videoProvider = AutoDisposeFutureProvider<List<Video>>.internal(
+  video,
+  name: r'videoProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$videoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef VideoRef = AutoDisposeFutureProviderRef<List<Video>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
