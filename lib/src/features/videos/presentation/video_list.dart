009a65dcb30aba76f2fbf893743ae540efd81fb4
AndresLince <lynxdarmatron@gmail.com>
2024-01-15 19:30:05 -0500

    Add video_list widget

---
+++ lib/src/features/videos/presentation/video_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone_flutter/src/features/videos/presentation/video_tile.dart';
+
+import '../domain/video.dart';
+
+class VideoList extends StatelessWidget {
+  final List<Video> videos;
+  const VideoList({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        Video video = videos[index];
+        return VideoTile(
+          name: video.name,
+          channelImage: video.channelImage,
+          views: video.views,
+          channelName: video.channelName,
+        );
+      },
+      itemCount: videos.length,
+    );
+  }
+}
