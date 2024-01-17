daf5f18a0d5bc6af7be81331e3a58ef192ba6435
AndresLince <lynxdarmatron@gmail.com>
2024-01-17 07:02:24 -0500

    Fix error on video_service

--- lib/src/features/videos/application/video_service.dart
+++ lib/src/features/videos/application/video_service.dart
@@ -3,22 +3,7 @@ import 'package:flutter/material.dart';
 import '../domain/video.dart';
 
 class VideoService extends ChangeNotifier{
-  List<Video> _videos = [
-    Video(
-      name: 'DevTalles PodCast - 147: Inicio 2024 | ¿Cómo hacer que te pasen cosas buenas?',
-      channelName: 'Fernando Herrera',
-      channelImage: 'https://yt3.ggpht.com/ytc/AIf8zZRUdgAqLyjIRxhCGaw_4Ln_awajey3DphahM-pQsg=s48-c-k-c0x00ffffff-no-rj',
-      views: '2,1 k visualizaciones',
-      videoUrl: ''
-    ),
-    Video(
-      name: 'DevTalles PodCast - 146: Fin del 2023 | Comparativa contra ti mismo',
-      channelName: 'Fernando Herrera',
-      channelImage: 'https://yt3.ggpht.com/ytc/AIf8zZRUdgAqLyjIRxhCGaw_4Ln_awajey3DphahM-pQsg=s48-c-k-c0x00ffffff-no-rj',
-      views: '2,3 k visualizaciones',
-      videoUrl: ''
-    ),
-  ];
+  List<Video> _videos = [];
 
   int get videosCount {
     return _videos.length;
