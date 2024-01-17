deb6f47073c96e3e1f0cf9010099059c2fcf1e36
AndresLince <lynxdarmatron@gmail.com>
2024-01-16 19:55:55 -0500

    Add details route

--- lib/src/routing/router.dart
+++ lib/src/routing/router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:youtube_clone_flutter/src/features/videos/presentation/recommended_videos.dart';
+import 'package:youtube_clone_flutter/src/features/videos/presentation/video_details_screen.dart';
 
 // GoRouter configuration
 final router = GoRouter(
@@ -7,6 +8,12 @@ final router = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => RecommendedVideos(),
+    ),
+    GoRoute(
+      path: '/video/:id',
+      builder: (context, state) => VideoDetailsScreen(
+        videoId: state.pathParameters['id'] ?? 'no-id',
+      ),
     ),
   ],
 );
